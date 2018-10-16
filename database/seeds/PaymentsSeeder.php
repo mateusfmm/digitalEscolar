274fe0de7eeefc34a7aaeb44a495a81acb3ac9d6
Júlia <juc.amarante@gmail.com>
2018-10-16 09:33:10 -0300

    Seeder

---
+++ database/seeds/PaymentsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PaymentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $payments = [
+
+        ];
+
+        DB::table('payments')->insert($payments);
+
+    }
+}
